885001880bf4fe32f955ea2e616ef265b9846f21
kebsont <mktapha@gmail.com>
2021-10-20 14:37:13 +0000

    basic for

---
+++ day1/basic_for.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var chaine string = "hello"
+	arr := [7]int{2, 5, 3, 77, 23, 1, 9}
+	for i := 0; i < len(arr); i++ {
+		fmt.Print(" ", arr[i])
+	}
+	fmt.Println()
+
+	for {
+		fmt.Println("Hey you")
+		fmt.Println("yes")
+		break
+		fmt.Println("Normally I won't appear")
+	}
+
+	for i := 0; i <= 5; i++ {
+		fmt.Println(i)
+		if i%2 == 0 {
+			continue
+		}
+		fmt.Println("I'll be printed")
+	}
+}
