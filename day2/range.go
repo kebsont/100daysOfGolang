8ddc265310b9d90ee481a9c41deaa7ede5626d3f
kebsont <mktapha@gmail.com>
2021-10-21 20:20:04 +0000

    range

---
+++ day2/range.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	sl := []string{"Lundi", "Mardi", "Mercredi", "Jeudi"}
+
+	for i, v := range sl {
+		fmt.Print(v)
+		fmt.Printf(" is %d ieme day\n", i+1)
+	}
+
+	my_map := map[string]string{"Name": "Salimata", "age": "43", "city": "Dakar"}
+	for k, v := range my_map {
+		fmt.Printf("%s => %s\n", k, v)
+	}
+
+	for k := range my_map {
+		fmt.Println(" Key: ", k)
+	}
+}
